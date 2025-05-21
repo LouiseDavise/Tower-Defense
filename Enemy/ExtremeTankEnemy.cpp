4dc52a185dd913e2739a54003aa194639df3d749
LouiseDavise <louizkwok@gmail.com>
2025-05-22 00:47:27 +0800

    ExtremeTankEnemy New Skill

--- Enemy/ExtremeTankEnemy.cpp
+++ Enemy/ExtremeTankEnemy.cpp
@@ -1,5 +1,43 @@
 #include <string>
 #include "ExtremeTankEnemy.hpp"
+#include "UI/Animation/ExplosionEffect.hpp"
+#include "Engine/Collider.hpp"
+#include "Engine/GameEngine.hpp"
+#include "Scene/PlayScene.hpp"
+#include "Engine/AudioHelper.hpp"
 
-ExtremeTankEnemy::ExtremeTankEnemy(int x, int y) : Enemy("play/enemy-5.png", x, y, 18, 30, 60, 8) {
+ExtremeTankEnemy::ExtremeTankEnemy(int x, int y)
+    : Enemy("play/enemy-5.png", x, y, 18, 30, 40, 8),
+      armorOverlay("play/armor-overlay.png", x, y)
+{
+    armorOverlay.Anchor = Anchor;
+    shieldToggleTimer = shieldToggleInterval;
+}
+
+void ExtremeTankEnemy::Hit(float damage)
+{
+    float actualDamage = shieldActive ? damage * (1.0f - damageReductionRatio) : damage;
+    Enemy::Hit(actualDamage);
+}
+
+void ExtremeTankEnemy::Update(float deltaTime)
+{
+    shieldToggleTimer -= deltaTime;
+    if (shieldToggleTimer <= 0)
+    {
+        shieldToggleTimer = shieldToggleInterval;
+        shieldActive = (rand() % 2 == 0);
+    }
+
+    armorOverlay.Position = Position;
+    Enemy::Update(deltaTime);
+}
+
+void ExtremeTankEnemy::Draw() const
+{
+    Enemy::Draw();
+    if (shieldActive)
+    {
+        armorOverlay.Draw();
+    }
 }
--- Enemy/ExtremeTankEnemy.hpp
+++ Enemy/ExtremeTankEnemy.hpp
@@ -2,8 +2,17 @@
 #define EXTREMETANKENEMY_HPP
 #include "Enemy.hpp"
 
-class ExtremeTankEnemy : public Enemy {
+class ExtremeTankEnemy : public Enemy
+{
 public:
+    bool shieldActive = true;
+    float shieldToggleTimer = 0.0f;
+    float shieldToggleInterval = 1.0f;
+    float damageReductionRatio = 1.0f;
+    Engine::Sprite armorOverlay;
     ExtremeTankEnemy(int x, int y);
+    void Hit(float damage) override;
+    void Update(float deltaTime) override;
+    void Draw() const override;
 };
-#endif   // EXTREMETANKENEMY_HPP
+#endif // EXTREMETANKENEMY_HPP
